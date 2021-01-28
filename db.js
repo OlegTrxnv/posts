fa6b32b97f951ed9127dff8763930155ea9c26cc
Oleg Trxnv <oleg.tarkhanov@gmail.com>
2021-01-28 17:01:02 +0200

    Complete

---
+++ db.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const mongodb = require("mongodb");
+
+mongodb.connect(
+  process.env.CONNECTIONSTRING,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  function (err, client) {
+    module.exports = client;
+    const app = require("./app");
+    app.listen(process.env.PORT || 3000);
+  }
+);
